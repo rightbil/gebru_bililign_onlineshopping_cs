4cc4a25066b51c4513d6766b69634898b66a3d0b
bgebru <rightbil@gmail.com>
2022-08-21 11:25:47 -0400

    Testing classes are added.

--- src/test/java/dev/cs/onlineshopping/controllers/ProductControllerTest.java
+++ src/test/java/dev/cs/onlineshopping/controllers/ProductControllerTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ProductControllerTest {
+    // controller does't make sense
+    // CRUD
     @BeforeEach
     void setUp() {
     }
