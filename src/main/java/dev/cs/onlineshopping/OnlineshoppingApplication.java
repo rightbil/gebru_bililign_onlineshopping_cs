c455e25f0081ce5e51475baa4a0aad626904a5a5
= <rightbil@gmail.com>
2025-03-15 16:38:34 -0400

    make app run at first time-1

--- src/main/java/dev/cs/onlineshopping/OnlineshoppingApplication.java
+++ src/main/java/dev/cs/onlineshopping/OnlineshoppingApplication.java
@@ -3,10 +3,13 @@ import dev.cs.onlineshopping.utility.Util;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import javax.persistence.Entity;
 import java.time.LocalDate;
 @Slf4j
 @SpringBootApplication
+@EntityScan()
 public class OnlineshoppingApplication {
     public static void main(String[] args) {
         System.out.println("[Spare Part online shopping App has started....] " + Util.orderDate());
--- target/classes/static/js/cs 2.js
+++
@@ -1,9 +0,0 @@
-function searchTerm(){
-    if(inputVal().length !=0)
-        window.document.location.href= "/product/search/"+ inputVal();
-    else
-        window.document.location.href= "/product"
-}
-function inputVal(){
-    return document.getElementById("input").value;
-}
